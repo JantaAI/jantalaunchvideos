009862a45d90f7e8452e940c644c1443bcb1a005
Konstantin Saifoulline <pytro@MacBook-Pro-by-Pytro.local>
2025-12-11 18:43:48 -0800

    Update hero headline to 'Video Production For Startups' and increase font size

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,7 @@ function Home() {
         </video>
         <div className="hero-section">
         <h1 className="hero-title">
-          Video production for startups
+          Video Production For Startups
         </h1>
           <p className="hero-tagline">
             Combining experiences with your product.
